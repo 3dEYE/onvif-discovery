199d7bde71b3e04fef2226c597cb698bde61a68c
Sergey Komisarchik <sergey.k@3deye.me>
2023-01-09 00:43:09 +0300

    cli tests

--- OnvifDiscovery.CLI/Program.cs
+++ OnvifDiscovery.CLI/Program.cs
@@ -3,39 +3,37 @@ using System.Threading;
 using System.Threading.Tasks;
 using OnvifDiscovery.Models;
 
+using Serilog;
+
 namespace OnvifDiscovery.CLI
 {
 	class Program
 	{
 		static async Task Main ()
 		{
-			Console.WriteLine ("Starting Discover ONVIF cameras for 10 seconds, press Ctrl+C to abort\n");
+			Log.Logger = new LoggerConfiguration ()
+				.WriteTo.Console ()
+				.CreateLogger();
+
+			Log.Information ("Starting Discover ONVIF cameras for 10 seconds, press Ctrl+C to abort\n");
 
 			var cts = new CancellationTokenSource ();
 			Console.CancelKeyPress += (s, e) => {
 				e.Cancel = true;
 				cts.Cancel ();
 			};
+
 			var discovery = new Discovery ();
 			await discovery.Discover (10, OnNewDevice, cts.Token);
-			Console.WriteLine ("ONVIF Discovery finished");
+
+			Log.Information ("ONVIF Discovery finished");
+
+			Log.CloseAndFlush ();
 		}
 
 		private static void OnNewDevice (DiscoveryDevice device)
 		{
-			// Multiple events could be received at the same time.
-			// The lock is here to avoid messing the console.
-			lock (Console.Out)
-			{
-				Console.WriteLine (
-					$"Device model {device.Model} from manufacturer {device.Mfr} has address {device.Address}");
-				Console.Write ($"Urls to device: ");
-				foreach (var address in device.XAdresses) {
-					Console.Write ($"{address}, ");
-				}
-
-				Console.WriteLine ("\n");
-			}
+			Log.Information ("Device discovered: {@device}", device);
 		}
 	}
 }
